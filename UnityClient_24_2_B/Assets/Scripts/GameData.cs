0fda84221b36449d4988ce585831d1d0d4c8e35e
shingugitvr000 <shingugitvr000@gmail.com>
2024-03-14 11:11:10 +0900

    [Feat] 스크립터블 Event 테스트 까지

---
+++ UnityClient_24_2_B/Assets/Scripts/GameData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New GameData" , menuName = "GameData", order = 50)]
+public class GameData : ScriptableObject
+{
+    public string gameName;
+    public int gameScore;
+    public bool isGmaeActive;       
+}
---
+++ UnityClient_24_2_B/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameData gameData;   
+    void Start()
+    {
+        //시작시 GameData 의 내역을 Debug.Log로 보여준다.
+        Debug.Log("Game Name : " + gameData.gameName);
+        Debug.Log("Game Score : " + gameData.gameScore);
+        Debug.Log("is Game Active : " + gameData.isGmaeActive);
+
+    }    
+}
