dca4745eb903986ce88dbbc02780f107431728d2
Shingugitvr000 <Shingugitvr000@gmail.com>
2024-05-02 10:11:52 +0900

    [Feat] GameSystem 변경 및 StoryModel 추가

--- UnityClient_24_2_B/Assets/Scripts/StoryGame/GameSystem.cs
+++ UnityClient_24_2_B/Assets/Scripts/StoryGame/GameSystem.cs
@@ -6,6 +6,7 @@ using System.Text;
 using UnityEngine.UI;           //UI
 using TMPro;                    //TextMeshPro
 using STORYGAME;
+using Unity.VisualScripting;
 
 namespace STROYGAME
 {
@@ -23,21 +24,6 @@ namespace STROYGAME
             {
                 gameSystem.ResetStoryModels();
             }
-
-            if(GUILayout.Button("Assing Text Component by Name"))
-            {
-                //오브젝트 이름으로 Text 컴포넌트를 찾기
-                GameObject textObject = GameObject.Find("StroyTextUI");
-                if(textObject != null) 
-                {
-                    Text textComponent = textObject.GetComponent<Text>();
-                    if(textComponent != null) 
-                    {
-                        //Text Componet 할당
-                        gameSystem.textComponent = textComponent;                       
-                    }
-                }
-            }
         }
     }
 #endif
@@ -46,15 +32,10 @@ namespace STROYGAME
     {
         public static GameSystem Instance;      //Scene 내부에서만 존재
 
-        public float delay = 0.1f;          //각 글자가 나타나는 시간
-        private string currentText = "";        //표시된 텍스트
-        public Text textComponent;          //TextMeshPro 컴포넌트
-
         private void Awake()
         {
             Instance = this;
         }
-
         public enum GAMESTATE
         {
             STORYSHOW,
@@ -62,21 +43,100 @@ namespace STROYGAME
             ENDMODE
         }
 
+        public GAMESTATE currentState;
+        public Stats stats;
+        public StoryModel[] storyModels;
+        public int currentStoryIndex = 0;
 
-        public GAMESTATE state;
-
-        public StoryTableObject[] storyModels;
-        public StoryTableObject currentModels;
-
-        private void Start()
+        public void ChangeState(GAMESTATE temp)
         {
-            currentModels = FindStoryModel(6);
-            StartCoroutine(ShowText());
+            currentState = temp;
+
+            if(currentState == GAMESTATE.STORYSHOW)
+            {
+                StoryShow(currentStoryIndex);
+            }
         }
 
-        StoryTableObject FindStoryModel(int number)
+        public void StoryShow(int number)
         {
-            StoryTableObject tempStoryModels = null;
+            StoryModel tempStroyModels = FindStoryModel(number);
+
+            //StorySystem.Instance.currentStoryModel = tempStroyModels;
+            //StorySystem.instance.CoShowText();
+        }
+
+        public void ApplyChoice(StoryModel.Result result)           //스토리 선택시 결과
+        {
+            switch(result.resultType)
+            {
+                case StoryModel.Result.ResultType.ChangeHp:
+
+                    //GameUI.Instance.UpdateHpUI()      //나중에 추가
+                    ChangeStats(result);
+                    break;
+
+                case StoryModel.Result.ResultType.GoToNextStory:
+                    currentStoryIndex = result.value;       //다음 이동 스토리 번호를 받아와서 실행
+                    ChangeState(GAMESTATE.STORYSHOW);
+                    ChangeStats(result);
+                    break;
+                case StoryModel.Result.ResultType.GoToRandomStory:
+                    RandomStory();
+                    ChangeState(GAMESTATE.STORYSHOW);
+                    ChangeStats(result);
+                    break;
+
+                default:
+                    Debug.LogError("UnKnown effect Type");
+                    break;
+            }
+        }
+
+        public void ChangeStats(StoryModel.Result result)           //상태 변경 함수
+        {
+            //기본 상태
+            if (result.stats.hpPoint > 0) stats.hpPoint += result.stats.hpPoint;
+            if (result.stats.spPoint > 0) stats.spPoint += result.stats.spPoint;
+            //현재 상태
+            if (result.stats.currentHpPoint > 0) stats.currentHpPoint += result.stats.currentHpPoint;
+            if (result.stats.currentSpPoint > 0) stats.currentSpPoint += result.stats.currentSpPoint;
+            if (result.stats.currentXpPoint > 0) stats.currentXpPoint += result.stats.currentXpPoint;
+            //능력치 상태
+            if (result.stats.strength > 0) stats.strength += result.stats.strength;
+            if (result.stats.dexterity > 0) stats.dexterity += result.stats.dexterity;
+            if (result.stats.consitiution > 0) stats.consitiution += result.stats.consitiution;
+            if (result.stats.wisdom > 0) stats.wisdom += result.stats.wisdom;
+            if (result.stats.Intelligence > 0) stats.Intelligence += result.stats.Intelligence;
+            if (result.stats.charisma > 0) stats.charisma += result.stats.charisma;
+        }
+
+
+        StoryModel RandomStory()                            //랜덤 스토리 리턴함수
+        {
+            StoryModel tempStoryModels = null;
+
+            List<StoryModel> StoryModelList = new List<StoryModel>();
+
+            for(int i = 0; i < storyModels.Length; i++)                     //MAIN 스토리들만 가져와서 List에 넣는다. 
+            {
+                if (storyModels[i].storyType == StoryModel.STORYTYPE.MAIN)
+                {
+                    StoryModelList.Add(storyModels[i]);
+                }
+            }
+
+            tempStoryModels = StoryModelList[Random.Range(0,StoryModelList.Count)]; //MAIN 들만 있는 리스트에서 랜덤으로 스토리 진행
+            currentStoryIndex = tempStoryModels.storyNumber;
+            Debug.Log("currentStoryIndex" + currentStoryIndex);
+
+            return tempStoryModels;
+        }
+
+        StoryModel FindStoryModel(int number)                   //스토리 번호로 찾아주는 함수 
+        {
+            StoryModel tempStoryModels = null;
+
             for(int i = 0; i < storyModels.Length; i++)
             {
                 if (storyModels[i].storyNumber == number)
@@ -88,24 +148,12 @@ namespace STROYGAME
             return tempStoryModels;
         }
 
-        IEnumerator ShowText()
-        {
-            for(int i = 0; i <= currentModels.storyText.Length; i++) 
-            {
-                currentText = currentModels.storyText.Substring(0, i);
-                textComponent.text = currentText;
-                yield return new WaitForSeconds(delay);
-            }
-            yield return new WaitForSeconds(delay);
-        }
-
-
 
 #if UNITY_EDITOR
         [ContextMenu("Reset Story Models")]
         public void ResetStoryModels()
         {
-            storyModels = Resources.LoadAll<StoryTableObject>("");
+            storyModels = Resources.LoadAll<StoryModel>("");
             //Resources 폴더 아래 모든 StoryModel 불러 오기
         }
 #endif
